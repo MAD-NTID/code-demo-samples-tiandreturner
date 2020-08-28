fe3c017f813741eb3e2beb9ccf420127a9d88cee
tiandreturner <tiandre.turner@outlook.com>
2020-08-28 16:49:25 -0400

    Adding the concept of parsing.

---
+++ ParsingInput/ParsingInput/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ParsingInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MINUTES_IN_HOUR = 60;
+            const double PI = 3.14159;
+            const string MESSAGE = "Bueno Dias";
+
+            // concatentation
+            Console.WriteLine("There are " + MINUTES_IN_HOUR +
+                " minutes in a hour.");
+
+            string aNumber = "42";
+            Console.WriteLine(aNumber + aNumber);
+
+            // this is string interpolaration
+            Console.WriteLine("There are {0} minutes in a hour.", MINUTES_IN_HOUR);
+            
+            //int number = 42;
+            //Console.WriteLine(number + number);
+
+            Console.WriteLine("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("You entered {0}", number + number);
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
