3c7f1dffc278644ecd01301b5aeb716ece180f24
tiandreturner <tiandre.turner@outlook.com>
2020-09-04 16:22:22 -0400

    Added Decisions and MoreDecisions

---
+++ Decisions/Decisions/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Decisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("10" == 10);
+
+            //const int AGE_TO_RENT_VEHICLE = 25;
+
+            //Console.Write("Enter your age: ");
+            //int age = int.Parse(Console.ReadLine());
+
+            //if (age >= AGE_TO_RENT_VEHICLE)
+            //{
+            //    Console.WriteLine("Due to your age, you do not need to pay for the surcharge");
+            //    Environment.Exit(0); // you can use this to quit the program.
+            //}
+
+            //Console.WriteLine("Due to your age, you will need to pay for the surcharge");
+
+            const string SECRET_PASSWORD = "MADstudentsAreAWESOME";
+            Console.WriteLine("Guess the password: ");
+            string password = Console.ReadLine();
+            if(password == SECRET_PASSWORD)
+            {
+                Console.WriteLine("You guessed correct!");
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("Sorry, it is not correct!");
+        }
+    }
+}
---
+++ MoreDecisions/MoreDecisions/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MoreDecisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int AGE_TO_RENT_VEHICLE = 25;
+            const int MIN_DRIVING_AGE = 16;
+
+            Console.Write("Enter your age: ");
+            //int age = int.Parse(Console.ReadLine());
+
+            //int age = 0;
+            if(int.TryParse(Console.ReadLine(), out int age))
+            {
+                Console.Write("Do you have a license? (y/n): ");
+                string hasLicense = Console.ReadLine();
+
+                // What does an if statement requires to have?
+                // boolean expression
+                if (age >= MIN_DRIVING_AGE && (hasLicense == "y" || hasLicense == "yes"))
+                {
+                    Console.WriteLine("You're permitted to drive.");
+                    //Console.WriteLine("Due to your age, you do not need to pay for the surcharge");
+                }
+                else
+                {
+                    Console.WriteLine("You're NOT permitted to drive.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("You entered an invalid input.");
+            }
+
+            
+
+        }
+    }
+}
