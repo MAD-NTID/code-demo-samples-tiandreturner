870896c1fae43c5ac45e7c890f4a308b4957d2b4
tiandreturner <tiandre.turner@outlook.com>
2020-09-23 15:56:17 -0400

    For Loop Demo

---
+++ ForLoops/ForLoops/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ForLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // increase by 1 from 0 to 4
+            // num++: num = num + 1
+            //for(int num = 0; num < 5; num++)
+            //{
+            //    Console.WriteLine(num);
+            //}
+
+            //for (int num = 0; num <= 5; )
+            //{
+            //    Console.WriteLine(num);
+            //    num++;
+            //}
+
+            // num--: num = num - 1
+            //for (int num = 20; num >= 0; num--)
+            //{
+            //    Console.WriteLine(num);
+            //}
+
+            // num+=5: num = num + 5
+            //for (int num = 0; num <= 100; num+=5)
+            //{
+            //    Console.WriteLine(num);
+            //}
+
+            // num+=num: num = num + num
+            //for (int num = 1; num < 10; num+=num)
+            //{
+            //    Console.WriteLine(num);
+            //}
+
+            //for (int num = 0; num <= 1000; num++)
+            //{
+            //    if(num % 25 == 0)
+            //    {
+            //        Console.WriteLine(num);
+            //    }
+            //}
+
+            //Console.Write("Enter a number: ");
+            //int endNum = int.Parse(Console.ReadLine());
+
+            //for (int num = 0; num <= endNum; num++)
+            //{
+            //    Console.WriteLine(num);
+            //}
+
+            Console.Write("Enter a start number: ");
+            int startNum = int.Parse(Console.ReadLine());
+
+            for (int num = startNum; num <= 100; num++)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
---
+++ ForLoops/ForLoopsWithArray/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ForLoopsWithArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] classList = {"Hy'suon Hill", "James Hughes", 
+                "Molly Mikkelson", "Joshua Howell-Leyer", "William Arnold"};
+
+            Console.WriteLine($"There are {classList.Length} students in the class");
+            for(int index = 0; index < classList.Length; index++)
+            {
+                Console.WriteLine(classList[index]);
+            }
+
+
+            Console.Write("Enter the number of new students to be added in the class: ");
+            int additionalStudents = int.Parse(Console.ReadLine());
+            int oldArraySize = classList.Length;
+
+            Array.Resize(ref classList, oldArraySize + additionalStudents);
+
+            for(int index = 0; index < additionalStudents; index++)
+            {
+                Console.WriteLine($"{index + 1}. Enter student's name: ");
+                classList[oldArraySize + index] = Console.ReadLine();
+            }
+
+            Console.WriteLine($"There are {classList.Length} students in the class");
+            for (int index = 0; index < classList.Length; index++)
+            {
+                Console.WriteLine(classList[index]);
+            }
+        }
+    }
+}
