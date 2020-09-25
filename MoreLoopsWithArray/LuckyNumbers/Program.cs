5d3c8b36e14271461559bef572e9884e46c01c2b
tiandreturner <tiandre.turner@outlook.com>
2020-09-25 16:34:27 -0400

    Add more loops

---
+++ MoreLoopsWithArray/LuckyNumbers/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace LuckyNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MAX_GUESS_ATTEMPTS = 5;
+            const int MAX_NUMBER = 50;
+
+            Random rand = new Random();
+            int[] luckyNumbers = new int[3];
+
+            for(int i = 0; i < luckyNumbers.Length; i++)
+            {
+                luckyNumbers[i] = rand.Next(MAX_NUMBER);
+            }
+            
+            //int currentGuess = 0;
+            //for (int i = 1; i <= MAX_GUESS_ATTEMPTS; i++)
+            //{
+            //    Console.Write($"Lucky guess #{i}: ");
+            //    currentGuess = int.Parse(Console.ReadLine());
+
+            //    for(int j = 0; j < luckyNumbers.Length; j++)
+            //    {
+            //        if(currentGuess == luckyNumbers[j])
+            //        {
+            //            Console.WriteLine("WOW! You got it right!");
+            //            Environment.Exit(0);
+            //        }
+            //    }
+            //}
+
+            Console.Write($"Aw shucks! the lucky numbers are ");
+            foreach (int luckyNumber in luckyNumbers)
+            {
+                Console.Write($"{luckyNumber} ");
+            }
+
+            int sum = 0;
+            for (int i = 0; i < luckyNumbers.Length; i++)
+            {
+                sum += luckyNumbers[i];
+            }
+
+            Console.WriteLine($"The sum of the lucky numbers is {sum}");
+
+
+
+            //int luckyNumber = rand.Next(MAX_NUMBER); // get a random number from 1 to max number (50)
+
+            //for(int i = 1; i <= MAX_GUESS_ATTEMPTS; i++)
+            //{
+            //    Console.Write($"Lucky guess #{i}: ");
+            //    currentGuess = int.Parse(Console.ReadLine());
+
+            //    if(currentGuess == luckyNumber)
+            //    {
+            //        Console.WriteLine("WOW! You got it right!");
+            //        Environment.Exit(0);
+            //    }
+            //    else if(currentGuess < luckyNumber)
+            //    {
+            //        Console.WriteLine("Aw, go a little bit higher");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Aw, go a little bit lower");
+            //    }
+            //}
+
+            // Console.WriteLine($"Aw shucks! the lucky number is {luckyNumber}");
+        }
+    }
+}
---
+++ MoreLoopsWithArray/MoreLoopsWithArray/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MoreLoopsWithArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] numArray = new int[10]; // creates an array of 10 elements that stores integers
+            //numArray[0] = (int)15.6; // casting 15.6 to become 15
+
+            Console.Write("How many students are in your class?: ");
+            int classSize = int.Parse(Console.ReadLine());
+
+            string[] classList = new string[classSize];
+            for(int i = 0; i < classList.Length; i++)
+            {
+                Console.Write($"Enter student {i + 1}: ");
+                classList[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Currently there are {classList.Length} students in the class");
+            for (int i = 0; i < classList.Length; i++)
+            {
+                Console.WriteLine(classList[i]);
+            }
+            //Console.Write("How many new students do you want to add to the classroom: ");
+            //int additionalStudents = int.Parse(Console.ReadLine());
+
+            //Array.Resize(ref classList, classList.Length + additionalStudents);
+
+            //Console.WriteLine($"Currently there are {classList.Length} students in the class");
+            //for (int i = 0; i < classList.Length; i++)
+            //{
+            //    Console.WriteLine(classList[i]);
+            //}
+        }
+    }
+}
