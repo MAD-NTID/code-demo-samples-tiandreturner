a2b6c493bb01d498d652ba7fe43a5b6771b2cb05
tiandreturner <tiandre.turner@outlook.com>
2020-10-02 15:49:17 -0400

    Uploading information about methods

---
+++ MoreMethods/MethodAnatomy/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MethodAnatomy
+{
+    class Program
+    {
+        public static string doSomething = "Do something...";
+        static  int number = 3;
+        static void Main(string[] args)
+        {
+            //Console.Write("Enter a number: ");
+            //int num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine(num1);
+            EnterNumber();
+            EnterNumber();
+
+           
+
+            //Console.Write("Enter a number: ");
+            //int num2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine(num2);
+        }
+
+        public static void DoSomething()
+        {
+            
+            Console.WriteLine(number);
+            Console.WriteLine(doSomething);
+        }
+
+        public static void EnterNumber()
+        {
+            Console.WriteLine(number);
+            Console.Write("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(num);
+        }
+    }
+}
---
+++ MoreMethods/ReturningMethods/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace ReturningMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DoSomething("Doing something");
+
+            //int num1 = EnterNumber();
+            //Console.WriteLine(num1);
+            //int num2 = EnterNumber();
+            //Console.WriteLine(num2);
+
+            //Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+
+            int num3 = EnterNumber();
+            int num4 = EnterNumber();
+
+            Console.WriteLine(DoPow(num3, num4));
+            Console.WriteLine(DoPow(num3, num4, out long result));
+            Console.WriteLine(result);
+
+        }
+
+        public static void DoSomething(string something)
+        {
+            Console.WriteLine(something);
+        }
+
+        public static void Shoot(string ball)
+        {
+            Console.WriteLine($"shooting {ball}");
+        }
+
+        public static int EnterNumber()
+        {
+            //Console.WriteLine(number);
+            Console.Write("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            return num;
+        }
+
+        public static int DoPow(int baseNumber, int power)
+        {
+            int result = 1;
+            for(int i = 1; i <= power; i++)
+            {
+                result *= baseNumber;
+            }
+
+            return result;
+        }
+
+
+
+        public static bool DoPow(int baseNumber, int power, out long result)
+        {
+            result = 1;
+            for (int i = 1; i <= power; i++)
+            {
+                result *= baseNumber;
+            }
+
+            if (result == 0)
+                return false;
+
+            return true;
+        }
+    }
+}
