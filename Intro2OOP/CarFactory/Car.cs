bd14cc57a9357d7733fd3a82af2d8cd9b349328b
tiandreturner <tiandre.turner@outlook.com>
2020-10-09 16:08:12 -0400

    Added constuctors and tostring

--- Intro2OOP/CarFactory/Car.cs
+++ Intro2OOP/CarFactory/Car.cs
@@ -25,6 +25,44 @@ namespace CarFactory
 
         public bool IsRunning { get; set; }
 
+        // default constructor
+        public Car()
+        {
+            Color = "White";
+            Windows = "Tinted Windows";
+            TypeOfTires = "Seaonal Tires";
+            HasGPS = false;
+            DoorType = "Door";
+            AntennaType = "Rod";
+            Price = 5000;
+            HasRadio = false;
+            Year = 2000;
+            CarType = "Truck";
+            Make = "Chevrolet";
+            Model = "Silverdo";
+            IsRunning = false;
+         }
+
+        // parameterized constructor
+        public Car(string _color, string _windows, string _typeOfTires, bool _hasGPS,
+            string _doorType, string _antennaType, double _price, bool _hasRadio,
+            int _year, string _carType, string _make, string _model, bool _isRunning)
+        {
+            Color = _color;
+            Windows = _windows;
+            TypeOfTires = _typeOfTires;
+            HasGPS = _hasGPS;
+            DoorType = _doorType;
+            AntennaType = _antennaType;
+            Price = _price;
+            HasRadio = _hasRadio;
+            Year = _year;
+            CarType = _carType;
+            Make = _make;
+            Model = _model;
+            IsRunning = _isRunning;
+        }
+
         public void Drive()
         {
             if (IsRunning)
@@ -35,6 +73,22 @@ namespace CarFactory
             {
                 Console.WriteLine("Cannot shift into the drive mode, the car is not running");
             }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Make: " + Make +
+                "\nModel: " + Model + 
+                $"\nColor: {Color}" +
+                $"\nPrice: {Price}" +
+                "\nYear: {0}", Year +
+                $"\nType of tires: {TypeOfTires}"  +
+                "\nHas GPS: " + HasGPS +
+                "\nHas Radio: " + HasRadio +
+                $"\nDoor Type: {DoorType}"  +
+                $"\nWindows Type: {Windows}"  +
+                $"\nCar Type: {CarType}" +
+                $"\nIs this car currently running: {IsRunning}");
         }
     }
 }
