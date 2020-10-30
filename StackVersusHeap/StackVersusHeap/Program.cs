0396f86d1509b5d5f3f3449f74a1e05ed8051fcc
tiandreturner <tiandre.turner@outlook.com>
2020-10-30 15:46:06 -0400

    Learning about Stack and Heap

---
+++ StackVersusHeap/StackVersusHeap/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace StackVersusHeap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 4;
+            CalcDogAge(ref age);
+
+            Console.WriteLine(age);
+            //string name = "Jane Doe";
+            ////string school = "RIT";
+            ////name = "John Doe";
+
+            //Person p1 = new Person() { Age = age, Name = name };
+            //Person p2 = new Person() { Age = 23, Name = "John Doe" };
+
+            //Console.WriteLine(p1.GetHashCode());
+            //Console.WriteLine(p2.GetHashCode());
+
+            //p2 = p1;
+            //Console.WriteLine(p2.GetHashCode());
+        }
+
+        public static int DoResult(int pValue)
+        {
+            int aValue = 5;
+            aValue = aValue * pValue;
+            return aValue;
+        }
+
+        public static void CalcDogAge(ref int age)
+        {
+            int dogAgeFactor = 7;
+            age = age * dogAgeFactor;
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
+
